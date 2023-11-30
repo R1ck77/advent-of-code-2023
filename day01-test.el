aca921e955eb818de873c0ac331f18e7810af133
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-11-30 16:40:48 +0100

    Add boilerplate code for Day 1

---
+++ day01-test.el
@@ -0,0 +1,18 @@
+(require 'day01)
+(require 'buttercup)
+
+(describe "Day 1"
+  (describe "part 1"
+    (it "replicates the example"
+      (expect (day01/part-1 (advent/read-problem-lines 1 :example))
+              :to-be 42))
+    (xit "solves the problem"
+      (expect (day01/part-1 (advent/read-problem-lines 1 :problem))
+              :to-be 42)))
+  (xdescribe "part 2"
+    (it "replicates the example"
+      (expect (day01/part-2 (advent/read-problem-lines 1 :example))
+              :to-be 42))
+    (xit "solves the problem"
+      (expect (day01/part-2 (advent/read-problem-lines 1 :problem))
+              :to-be 42))))
