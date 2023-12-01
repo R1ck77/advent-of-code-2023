32f17832b4da4cd49d8e52f136ac8e4b9f9f938e
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-12-01 11:15:41 +0100

    You tricky bas$$$d! :D

--- day01-test.el
+++ day01-test.el
@@ -10,9 +10,17 @@
       (expect (day01/part-1 (advent/read-problem-lines 1 :problem))
               :to-be 54953)))
   (describe "part 2"
+    (it "replaces each string to numbers correctly"
+      (expect (day01/replace-english-numbers "two1nine") :to-equal "219")
+      (expect (day01/replace-english-numbers "eightwothree") :to-equal "8wo3")
+      (expect (day01/replace-english-numbers "abcone2threexyz") :to-equal "abc123xyz")
+      (expect (day01/replace-english-numbers "xtwone3four") :to-equal "x2ne34")
+      (expect (day01/replace-english-numbers "4nineeightseven2") :to-equal "49872")
+      (expect (day01/replace-english-numbers "zoneight234") :to-equal "z1ight234")
+      (expect (day01/replace-english-numbers "7pqrstsixteen") :to-equal "7pqrst6teen"))
     (it "replicates the example"
       (expect (day01/part-2 (advent/read-problem-lines 1 :example 2))
               :to-be 281))
-    (xit "solves the problem"
+    (it "solves the problem"
       (expect (day01/part-2 (advent/read-problem-lines 1 :problem))
-              :to-be 42))))
+              :to-be 53885))))
--- day01.el
+++ day01.el
@@ -2,6 +2,17 @@
 (require 'advent-utils)
 (require 's)
 
+;; Zero doesn't seem to be present…
+(defconst string-to-digit '(("one" "1")
+                            ("two" "2")
+                            ("three" "3")
+                            ("four" "4")
+                            ("five" "5")
+                            ("six" "6")
+                            ("seven" "7")
+                            ("eight" "8")
+                            ("nine" "9")))
+
 (defun day01/-get-numbers (s)
   (-map #'string-to-number
         (-map #'char-to-string
@@ -25,7 +36,38 @@
          (-map #'day01/-get-extremes
                (-map #'day01/-get-numbers lines)))))
 
+(defun day01/-next-replacement ()
+  (cadr
+   (car
+    (--sort (< (car it )
+               (car other))
+            (--filter it
+                      (--map (progn
+                               (goto-char (point-min))
+                               (if (search-forward (car it) nil t)
+                                   (list (match-beginning 0) it)))
+                             string-to-digit))))))
+
+(defun day01/-replace-all ()
+  (let ((next-replacement (day01/-next-replacement)))
+    (while next-replacement
+      (goto-char (point-min))
+      (search-forward (car next-replacement))
+      (replace-match (cadr next-replacement))
+      (setq next-replacement (day01/-next-replacement)))))
+
+(defun day01/replace-english-numbers (line)
+  (with-temp-buffer
+    (insert line)
+    (day01/-replace-all)
+    (buffer-substring (point-min) (point-max))))
+
+
+(defun day01/-replace-numerical-strings (lines)
+  (-map #'day01/replace-english-numbers lines))
+
 (defun day01/part-2 (lines)
-  (error "Not yet implemented"))
+  (day01/part-1
+   (day01/-replace-numerical-strings lines)))
 
 (provide 'day01)
