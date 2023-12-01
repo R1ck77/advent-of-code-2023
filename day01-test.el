09ba9dfc077ca06bca8af4864ac38b8a215e70da
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-12-01 10:16:36 +0100

    Day 1 part 1 solved

--- day01-test.el
+++ day01-test.el
@@ -1,14 +1,14 @@
 (require 'day01)
 (require 'buttercup)
 
-(describe "Day 1"
+(describe "--- Day 1: Trebuchet?! ---"
   (describe "part 1"
     (it "replicates the example"
       (expect (day01/part-1 (advent/read-problem-lines 1 :example))
-              :to-be 42))
-    (xit "solves the problem"
+              :to-be 142))
+    (it "solves the problem"
       (expect (day01/part-1 (advent/read-problem-lines 1 :problem))
-              :to-be 42)))
+              :to-be 54953)))
   (xdescribe "part 2"
     (it "replicates the example"
       (expect (day01/part-2 (advent/read-problem-lines 1 :example))
--- day01.el
+++ day01.el
@@ -2,8 +2,28 @@
 (require 'advent-utils)
 (require 's)
 
+(defun day01/-get-numbers (s)
+  (-map #'string-to-number
+        (-map #'char-to-string
+              (--filter (and (> it 47)
+                             (< it 58))
+                        (--map it s)))))
+
+(defun day01/-get-extremes (l)
+  (list (car l) (car (reverse l))))
+
+(defun day01/-number-from-extremes (pair)
+  (+ (* 10 (car pair))
+     (cadr pair)))
+
+(defun day01/-add-all (l)
+  (apply #'+ l))
+
 (defun day01/part-1 (lines)
-  (error "Not yet implemented"))
+  (day01/-add-all
+   (-map #'day01/-number-from-extremes
+         (-map #'day01/-get-extremes
+               (-map #'day01/-get-numbers lines)))))
 
 (defun day01/part-2 (lines)
   (error "Not yet implemented"))
