6aaeb6d01e05b6dd61b3e312dfbd7c1a903bfcb2
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-12-01 10:47:04 +0100

    Add boilerplate for part 2

--- day01-test.el
+++ day01-test.el
@@ -9,10 +9,10 @@
     (it "solves the problem"
       (expect (day01/part-1 (advent/read-problem-lines 1 :problem))
               :to-be 54953)))
-  (xdescribe "part 2"
+  (describe "part 2"
     (it "replicates the example"
-      (expect (day01/part-2 (advent/read-problem-lines 1 :example))
-              :to-be 42))
+      (expect (day01/part-2 (advent/read-problem-lines 1 :example 2))
+              :to-be 281))
     (xit "solves the problem"
       (expect (day01/part-2 (advent/read-problem-lines 1 :problem))
               :to-be 42))))
