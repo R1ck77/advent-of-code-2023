f3d33823dd197c597fd1dc34e131fa5285ee87a5
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-12-02 11:20:51 +0100

    Day 2 part 1 solved

--- day02-test.el
+++ day02-test.el
@@ -5,10 +5,10 @@
   (describe "part 1"
     (it "replicates the example"
       (expect (day02/part-1 (advent/read-problem-lines 2 :example))
-              :to-be 42))
-    (xit "solves the problem"
+              :to-be 8))
+    (it "solves the problem"
       (expect (day02/part-1 (advent/read-problem-lines 2 :problem))
-              :to-be 42)))
+              :to-be 2169)))
   (xdescribe "part 2"
     (it "replicates the example"
       (expect (day02/part-2 (advent/read-problem-lines 2 :example))
--- day02.el
+++ day02.el
@@ -2,8 +2,42 @@
 (require 'advent-utils)
 (require 's)
 
+(defun day02/add-draw (draw-table color-string)
+  (pcase (s-match "[0-9]+ \\(red\\|green\\|blue\\)" color-string)
+    (`(,svalue ,color) (advent/put draw-table color (string-to-number svalue)))))
+
+(defun day02/parse-draw (sdraw)
+  (let ((draw-table (advent/table)))
+    (--each (s-split "," sdraw)
+      (day02/add-draw draw-table it))
+    (list (advent/get draw-table "red" 0)
+          (advent/get draw-table "green" 0)
+          (advent/get draw-table "blue" 0))))
+
+(defun day02/parse-draws (draws)
+  "String of draws -> list of 3-tuples"
+  (-map #'day02/parse-draw (s-split ";" draws)))
+
+(defun day02/read-game (line)
+  "Line -> id, followed by a list of 3 tuples"
+  (pcase (s-match "^Game \\([0-9]+\\): \\(.*\\)" line)
+    (`(,unused ,sid ,draws) (list (string-to-number sid)
+                                  (day02/parse-draws draws)))))
+
+(defun day02/read-games (lines)
+  (-map #'day02/read-game lines))
+
+(defun day02/is-valid-draw (draw max-cubes)
+  (not (--filter (> (car it) (cdr it)) (-zip draw max-cubes))))
+
+(defun day02/is-valid-game (game &optional max-cubes)
+  (let ((max-cubes (or max-cubes '(12 13 14))))
+    (--all? (day02/is-valid-draw it max-cubes) (cadr game))))
+
 (defun day02/part-1 (lines)
-  (error "Not yet implemented"))
+  (apply #'+
+         (-map #'car (-filter #'day02/is-valid-game
+                        (day02/read-games lines)))))
 
 (defun day02/part-2 (lines)
   (error "Not yet implemented"))
