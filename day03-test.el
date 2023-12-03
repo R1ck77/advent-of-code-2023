cda705566b1d2a0e51cfc9d3a741181d4a05adfe
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-12-03 12:51:14 +0100

    Add day 3 data

--- day03-test.el
+++ day03-test.el
@@ -1,11 +1,11 @@
 (require 'day03)
 (require 'buttercup)
 
-(describe "Day 3"
+(describe "--- Day 3: Gear Ratios ---"
   (describe "part 1"
     (it "replicates the example"
       (expect (day03/part-1 (advent/read-problem-lines 3 :example))
-              :to-be 42))
+              :to-be 4361))
     (xit "solves the problem"
       (expect (day03/part-1 (advent/read-problem-lines 3 :problem))
               :to-be 42)))
