651664525e68473d696cb6155a5eb60011f99da4
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-12-03 17:06:50 +0100

    Add boilerplate files for day 4

---
+++ day04-test.el
@@ -0,0 +1,18 @@
+(require 'day04)
+(require 'buttercup)
+
+(describe "Day 4"
+  (describe "part 1"
+    (it "replicates the example"
+      (expect (day04/part-1 (advent/read-problem-lines 4 :example))
+              :to-be 42))
+    (xit "solves the problem"
+      (expect (day04/part-1 (advent/read-problem-lines 4 :problem))
+              :to-be 42)))
+  (xdescribe "part 2"
+    (it "replicates the example"
+      (expect (day04/part-2 (advent/read-problem-lines 4 :example))
+              :to-be 42))
+    (xit "solves the problem"
+      (expect (day04/part-2 (advent/read-problem-lines 4 :problem))
+              :to-be 42))))
---
+++ day04.el
@@ -0,0 +1,11 @@
+(require 'dash)
+(require 'advent-utils)
+(require 's)
+
+(defun day04/part-1 (lines)
+  (error "Not yet implemented"))
+
+(defun day04/part-2 (lines)
+  (error "Not yet implemented"))
+
+(provide 'day04)
