aebe1785b87f8ea21f71a06979e3c56d47b72368
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-12-04 10:45:00 +0100

    Data read

--- day04.el
+++ day04.el
@@ -2,6 +2,29 @@
 (require 'advent-utils)
 (require 's)
 
+(defun day04/-read-numbers-list (token)
+  (-map #'string-to-number (s-split " " token t)))
+
+(defun day04/-read-given-numbers (token)
+  (day04/-read-numbers-list token))
+
+(defun day04/-read-winning-numbers (token)
+  (day04/-read-numbers-list token))
+
+(defun day04/-read-card-number (token)
+  (string-to-number
+   (elt (s-match "Card +\\([0-9]+\\)" token) 1)))
+
+(defun day04/-split-tokens (line)
+  (-map #'s-trim (s-split "|" (s-replace ":" "|" line))))
+
+(defun day04/read-data (lines)
+  (--map (list
+          (day04/-read-card-number (elt it 0))
+          (day04/-read-winning-numbers (elt it 1))
+          (day04/-read-given-numbers (elt it 2)))
+         (-map #'day04/-split-tokens lines)))
+
 (defun day04/part-1 (lines)
   (error "Not yet implemented"))
 
