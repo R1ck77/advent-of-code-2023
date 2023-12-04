74da97f7572bad6048680b046cf34a87d683fc35
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-12-04 10:29:23 +0100

    Add data for day 4

--- day04-test.el
+++ day04-test.el
@@ -5,7 +5,7 @@
   (describe "part 1"
     (it "replicates the example"
       (expect (day04/part-1 (advent/read-problem-lines 4 :example))
-              :to-be 42))
+              :to-be 13))
     (xit "solves the problem"
       (expect (day04/part-1 (advent/read-problem-lines 4 :problem))
               :to-be 42)))
