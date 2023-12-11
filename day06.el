944e6034a826fec1e72fda009363729c1ecc59c1
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-12-11 15:04:35 +0100

    Day 6 part 1 solved

--- day06.el
+++ day06.el
@@ -2,8 +2,31 @@
 (require 'advent-utils)
 (require 's)
 
+(defun day06/distance (total-time push-time)
+  (let ((move-time (- total-time push-time)))
+    (* (max 0 move-time) push-time)))
+
+(defun day06/-read-line (line)  
+  (-map #'string-to-number
+        (s-split " "
+                 (cadr (s-split ":" line t)) t)))
+
+(defun day06/read-data (lines)
+  (-zip (day06/-read-line (car lines))
+        (day06/-read-line (cadr lines))))
+
+(defun day06/compute-win-chances (time+record)
+  (let ((total-time (car time+record))
+        (record-distance (cdr time+record)))
+    (length
+     (--filter (> it record-distance)
+               (--map (day06/distance total-time it)
+                      (number-sequence 0 total-time))))))
+
 (defun day06/part-1 (lines)
-  (error "Not yet implemented"))
+  (apply #'*
+         (-map #'day06/compute-win-chances
+         (day06/read-data lines))))
 
 (defun day06/part-2 (lines)
   (error "Not yet implemented"))
