045ef7f38c532bc2881a3c143d2567d83d48c0f1
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-12-11 15:04:35 +0100

    Add data for day 7

--- day07-test.el
+++ day07-test.el
@@ -1,11 +1,11 @@
 (require 'day07)
 (require 'buttercup)
 
-(describe "Day 7"
+(describe "--- Day 7: Camel Cards ---"
   (describe "part 1"
     (it "replicates the example"
       (expect (day07/part-1 (advent/read-problem-lines 7 :example))
-              :to-be 42))
+              :to-be 6440))
     (xit "solves the problem"
       (expect (day07/part-1 (advent/read-problem-lines 7 :problem))
               :to-be 42)))
