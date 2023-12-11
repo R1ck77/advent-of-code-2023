7323fedcb0ab9995a0e2bd6a23021fde74ed32a9
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-12-11 15:04:35 +0100

    Data for day 10 added
    Oh God, I need to reimplement Dijkstra again (and the fact that I know how
    to write "Dijkstra" by heart says something…)

--- day10-test.el
+++ day10-test.el
@@ -1,18 +1,21 @@
 (require 'day10)
 (require 'buttercup)
 
-(describe "Day 10"
+(defun day10/-to-symbol (s)
+  (intern (concat ":" s)))
+
+(describe "--- Day 10: Pipe Maze ---"
   (describe "part 1"
     (it "replicates the example"
-      (expect (day10/part-1 (advent/read-problem-lines 10 :example))
-              :to-be 42))
+      (expect (day10/part-1 (advent/read-grid 10 :example #'day10/-to-symbol))
+              :to-be 8))
     (xit "solves the problem"
-      (expect (day10/part-1 (advent/read-problem-lines 10 :problem))
+      (expect (day10/part-1 (advent/read-grid 10 :problem #'day10/-to-symbol))
               :to-be 42)))
   (xdescribe "part 2"
     (it "replicates the example"
-      (expect (day10/part-2 (advent/read-problem-lines 10 :example))
+      (expect (day10/part-2 (advent/read-grid 10 :example #'day10/-to-symbol))
               :to-be 42))
     (xit "solves the problem"
-      (expect (day10/part-2 (advent/read-problem-lines 10 :problem))
+      (expect (day10/part-2 (advent/read-grid 10 :problem #'day10/-to-symbol))
               :to-be 42))))
