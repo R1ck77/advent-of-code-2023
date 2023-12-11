adac42c204486852462b6d4ea0141a1c0ca6fad6
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-12-11 15:04:36 +0100

    Add data for day 11

--- day11-test.el
+++ day11-test.el
@@ -1,18 +1,21 @@
 (require 'day11)
 (require 'buttercup)
 
-(describe "Day 11"
+(defun day11/-to-symbol (s)
+  (intern (concat ":" s)))
+
+(describe "--- Day 11: Cosmic Expansion ---"
   (describe "part 1"
     (it "replicates the example"
-      (expect (day11/part-1 (advent/read-problem-lines 11 :example))
-              :to-be 42))
+      (expect (day11/part-1 (advent/read-grid 11 :example #'day11/-to-symbol))
+              :to-be 374))
     (xit "solves the problem"
-      (expect (day11/part-1 (advent/read-problem-lines 11 :problem))
+      (expect (day11/part-1 (advent/read-grid 11 :problem #'day11/-to-symbol))
               :to-be 42)))
   (xdescribe "part 2"
     (it "replicates the example"
-      (expect (day11/part-2 (advent/read-problem-lines 11 :example))
+      (expect (day11/part-2 (advent/read-grid 11 :example #'day11/-to-symbol))
               :to-be 42))
     (xit "solves the problem"
-      (expect (day11/part-2 (advent/read-problem-lines 11 :problem))
+      (expect (day11/part-2 (advent/read-grid 11 :problem #'day11/-to-symbol))
               :to-be 42))))
