8db4763bf1484d69c210522cb144f533725171cc
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-12-11 15:04:35 +0100

    Add data for day 8

--- day08-test.el
+++ day08-test.el
@@ -1,11 +1,11 @@
 (require 'day08)
 (require 'buttercup)
 
-(describe "Day 8"
+(describe "--- Day 8: Haunted Wasteland ---"
   (describe "part 1"
     (it "replicates the example"
       (expect (day08/part-1 (advent/read-problem-lines 8 :example))
-              :to-be 42))
+              :to-be 6))
     (xit "solves the problem"
       (expect (day08/part-1 (advent/read-problem-lines 8 :problem))
               :to-be 42)))
