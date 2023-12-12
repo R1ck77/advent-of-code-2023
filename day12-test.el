9e828e3eded1d19c9369e2a63849b5741f677385
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2023-12-12 11:52:01 +0100

    Add data for day 12

--- day12-test.el
+++ day12-test.el
@@ -1,11 +1,29 @@
 (require 'day12)
 (require 'buttercup)
 
-(describe "Day 12"
+(describe "--- Day 12: Hot Springs ---"
   (describe "part 1"
+    (it "computes the right arrangements for '???.### 1,1,3'"
+      (expect (day12/get-arrangements (day12/read-line "???.### 1,1,3"))
+              :to-be 1))
+    (it "computes the right arrangements for '.??..??...?##. 1,1,3'"
+      (expect (day12/get-arrangements (day12/read-line ".??..??...?##. 1,1,3"))
+              :to-be 4))
+    (it "computes the right arrangements for '?#?#?#?#?#?#?#? 1,3,1,6'"
+      (expect (day12/get-arrangements (day12/read-line "?#?#?#?#?#?#?#? 1,3,1,6"))
+              :to-be 1))
+    (it "computes the right arrangements for '????.#...#... 4,1,1'"
+      (expect (day12/get-arrangements (day12/read-line "????.#...#... 4,1,1"))
+              :to-be 1))
+    (it "computes the right arrangements for '????.######..#####. 1,6,5'"
+      (expect (day12/get-arrangements (day12/read-line "????.######..#####. 1,6,5"))
+              :to-be 4))
+    (it "computes the right arrangements for '?###???????? 3,2,1'"
+      (expect (day12/get-arrangements (day12/read-line "?###???????? 3,2,1"))
+              :to-be 10))
     (it "replicates the example"
       (expect (day12/part-1 (advent/read-problem-lines 12 :example))
-              :to-be 42))
+              :to-be 21))
     (xit "solves the problem"
       (expect (day12/part-1 (advent/read-problem-lines 12 :problem))
               :to-be 42)))
--- day12.el
+++ day12.el
@@ -2,6 +2,12 @@
 (require 'advent-utils)
 (require 's)
 
+(defun day12/read-line (line)
+  )
+
+(defun day12/get-arrangements (data)
+  )
+
 (defun day12/part-1 (lines)
   (error "Not yet implemented"))
 
