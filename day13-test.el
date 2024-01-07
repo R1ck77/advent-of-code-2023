27608d9b446a78cc2c03743368ee58a0528ec5f7
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2024-01-07 17:02:20 +0100

    Add boilerplate files for day 13

---
+++ day13-test.el
@@ -0,0 +1,18 @@
+(require 'day13)
+(require 'buttercup)
+
+(describe "Day 13"
+  (describe "part 1"
+    (it "replicates the example"
+      (expect (day13/part-1 (advent/read-problem-lines 13 :example))
+              :to-be 42))
+    (xit "solves the problem"
+      (expect (day13/part-1 (advent/read-problem-lines 13 :problem))
+              :to-be 42)))
+  (xdescribe "part 2"
+    (it "replicates the example"
+      (expect (day13/part-2 (advent/read-problem-lines 13 :example))
+              :to-be 42))
+    (xit "solves the problem"
+      (expect (day13/part-2 (advent/read-problem-lines 13 :problem))
+              :to-be 42))))
