4c175e767dfef4931ed53708bfce709a77f4d669
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2024-01-08 10:31:27 +0100

    Add data for day 13

--- day13-test.el
+++ day13-test.el
@@ -1,11 +1,11 @@
 (require 'day13)
 (require 'buttercup)
 
-(describe "Day 13"
+(describe "--- Day 13: Point of Incidence ---"
   (describe "part 1"
     (it "replicates the example"
       (expect (day13/part-1 (advent/read-problem-lines 13 :example))
-              :to-be 42))
+              :to-be 405))
     (xit "solves the problem"
       (expect (day13/part-1 (advent/read-problem-lines 13 :problem))
               :to-be 42)))
