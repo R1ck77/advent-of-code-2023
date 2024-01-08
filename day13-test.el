0ead32fce2a304625f578fcd04a4d77147e06c67
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2024-01-08 12:29:37 +0100

    Add data for part 2

--- day13-test.el
+++ day13-test.el
@@ -9,10 +9,10 @@
     (it "solves the problem"
       (expect (day13/part-1 (advent/read-blocks-of-lines 13 :problem))
               :to-be 36041)))
-  (xdescribe "part 2"
+  (describe "part 2"
     (it "replicates the example"
       (expect (day13/part-2 (advent/read-blocks-of-lines 13 :example))
-              :to-be 42))
+              :to-be 400))
     (xit "solves the problem"
       (expect (day13/part-2 (advent/read-blocks-of-lines 13 :problem))
               :to-be 42))))
