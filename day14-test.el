9e1390846b92660a45d00dd64363ff926561eb2e
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2024-01-09 22:58:06 +0100

    Add data for day 14

--- day14-test.el
+++ day14-test.el
@@ -1,11 +1,11 @@
 (require 'day14)
 (require 'buttercup)
 
-(describe "Day 14"
+(describe "--- Day 14: Parabolic Reflector Dish ---"
   (describe "part 1"
     (it "replicates the example"
       (expect (day14/part-1 (advent/read-problem-lines 14 :example))
-              :to-be 42))
+              :to-be 136))
     (xit "solves the problem"
       (expect (day14/part-1 (advent/read-problem-lines 14 :problem))
               :to-be 42)))
