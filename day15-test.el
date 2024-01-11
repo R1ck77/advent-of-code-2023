f180caff97cb7bdff35b681401e8328d522f0362
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2024-01-11 21:05:59 +0100

    Add test for day 15 part 2 example

--- day15-test.el
+++ day15-test.el
@@ -9,10 +9,10 @@
     (it "solves the problem"
       (expect (day15/part-1 (advent/read-problem-lines 15 :problem))
               :to-be 514639)))
-  (xdescribe "part 2"
+  (describe "part 2"
     (it "replicates the example"
       (expect (day15/part-2 (advent/read-problem-lines 15 :example))
-              :to-be 42))
+              :to-be 145))
     (xit "solves the problem"
       (expect (day15/part-2 (advent/read-problem-lines 15 :problem))
               :to-be 42))))
