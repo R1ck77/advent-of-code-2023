9d6de3eef33605f2ea5972bdfbae2e8275876196
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2024-01-11 20:52:59 +0100

    Day 15 part 1 solved

--- day15.el
+++ day15.el
@@ -2,8 +2,17 @@
 (require 'advent-utils)
 (require 's)
 
+(defun day15/read-data (lines)
+  (s-split "," (car lines) t))
+
+(defun day15/compute-hash (s)
+  (--reduce-from (% (* (+ acc it) 17) 256) 0  (string-to-list s)))
+
+(defun day15/compute-total-hash (xs)
+  (apply #'+ (-map #'day15/compute-hash xs)))
+
 (defun day15/part-1 (lines)
-  (error "Not yet implemented"))
+  (day15/compute-total-hash (day15/read-data lines)))
 
 (defun day15/part-2 (lines)
   (error "Not yet implemented"))
