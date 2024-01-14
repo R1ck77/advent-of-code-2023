0cdb99e7c5207d83428cf6aac93875e17d7b1fb3
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2024-01-14 13:28:23 +0100

    Add data for day 16

--- day16-test.el
+++ day16-test.el
@@ -1,11 +1,11 @@
 (require 'day16)
 (require 'buttercup)
 
-(describe "Day 16"
+(describe "--- Day 16: The Floor Will Be Lava ---"
   (describe "part 1"
     (it "replicates the example"
       (expect (day16/part-1 (advent/read-problem-lines 16 :example))
-              :to-be 42))
+              :to-be 46))
     (xit "solves the problem"
       (expect (day16/part-1 (advent/read-problem-lines 16 :problem))
               :to-be 42)))
